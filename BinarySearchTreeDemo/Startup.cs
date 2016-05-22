75481a7b8dad5c52f976edf3416decc05fbef252
VoNhatSinh <nhatsinh18@gmail.com>
2016-05-22 16:41:33 +0700

    Sinh-22/05/2016-Firsr version Website

---
+++ BinarySearchTreeDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BinarySearchTreeDemo.Startup))]
+namespace BinarySearchTreeDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
