346e90c843b933972d28ed31dfabc102dcacef7a
VoNhatSinh <nhatsinh18@gmail.com>
2016-05-22 21:43:34 +0700

    Sinh-22/05/2016

--- BinarySearchTreeDemo/App_Start/FilterConfig.cs
+++ BinarySearchTreeDemo/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace BinarySearchTreeDemo
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            
         }
     }
 }
