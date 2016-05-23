18c1047523c416302f2fbad6c51bb0e3eb25abb8
VoNhatSinh <nhatsinh18@gmail.com>
2016-05-23 22:36:34 +0700

    Sinh-Common Graphic

---
+++ BinarySearchTreeDemoApp/GraphicsLib/GdiPlusGraphics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeDemoApp.GraphicsLib
+{
+    public class GdiPlusGraphics : CommonGraphics
+    {
+        private Graphics graphics;
+
+        private Pen pen;
+
+        public Pen Pen
+        {
+            get { return pen; }
+            set { pen = value; }
+        }
+
+
+        public GdiPlusGraphics(Graphics graphics)
+        {
+            this.graphics = graphics;
+            this.pen = null;
+        }
+
+        public GdiPlusGraphics(Graphics graphics, Pen pen)
+        {
+            this.graphics = graphics;
+            this.pen = pen;
+        }
+
+    }
+}
