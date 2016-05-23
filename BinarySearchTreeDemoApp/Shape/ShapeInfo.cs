d654f003a28219c9c36f4e3d7b626aa44175ee5c
VoNhatSinh <nhatsinh18@gmail.com>
2016-05-23 23:04:09 +0700

    Sinh - Add content shapInfo

--- BinarySearchTreeDemoApp/Shape/ShapeInfo.cs
+++ BinarySearchTreeDemoApp/Shape/ShapeInfo.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
 namespace BinarySearchTreeDemoApp.Shape
 {
     public class ShapeInfo
     {
+        private string Content;
+        private Point position;
     }
 }
